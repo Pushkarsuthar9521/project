085082d10feef1b95cbfecd81a78421c9984c3bf
Pushkarsuthar9521 <pushkar.suthar9521@gmail.com>
2023-08-14 18:28:26 +0530

    new commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+
+
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+      <div>
+         <Navbar/>
+         <Home/>
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import heroImage from "../assets/hero.PNG"
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
