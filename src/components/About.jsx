33b646e6a80ca5e37a66991f032e40d994a8ad00
Pushkarsuthar9521 <pushkar.suthar9521@gmail.com>
2023-08-27 23:10:13 +0530

    ddg

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+    <div name='about' className='w-full min-h-screen h-auto bg-gradient-to-b from-gray-800 to-black text-white pb-10'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
 
@@ -10,7 +10,7 @@ const About = () => {
 
             </div>
 
-            <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat soluta itaque accusantium deleniti quia harum inventore facere at, officia aspernatur deserunt minus autem neque officiis obcaecati, pariatur mollitia cum. Praesentium minima quos, optio hic ducimus quo consequatur veniam voluptas, itaque delectus, perferendis enim. Nisi vero error incidunt saepe inventore qui!</p>
+            <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat soluta itaque accusantium deleniti quia harum inventore facere at, officia  qui!</p>
 
             <br />
 
